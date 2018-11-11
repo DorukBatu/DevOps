b49abb656575aa2c9f5de78c5a722887e3ee5d3f
doruk.batu <doruk.batu@outlook.com>
2018-11-11 07:33:30 -0800

    first commit

---
+++ selenium/selenium/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com/ncr");
+            Console.WriteLine(driver.Title);
+
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("DevOps");
+            query.Submit();
+            Console.WriteLine(driver.Title);
+
+            driver.Quit();
+
+        }
+    }
+}
